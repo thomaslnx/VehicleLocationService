0e7bc2b2487f1adb44efc598de2a106b7f1ab8df
thomaslnx <marcos.msilva10@gmail.com>
2023-09-21 21:35:41 -0300

    Add reportCurrentLocation method for the vehicles reports their current locations Add README.md skeleton Refactor server.ts

--- src/controllers/location-monitoring/getVehicleLocation.ts
+++ src/controllers/location-monitoring/getVehicleLocation.ts
@@ -20,6 +20,41 @@ class Vehicle {
   }
 }
 
+export const reportCurrentLocation = (req: Request, res: Response) => {
+  const { vehicleId, latitude, longitude } = req.body
+  if (!vehicleId || !latitude || !longitude) {
+    return res.status(400).json({ error: "Request data invalid!"})
+  }
+
+  let vehicles = vehicleData.map(vehicle => {
+    return new Vehicle(vehicle.currentPosition, vehicle.path, vehicle.vehicleId)
+  })
+
+  // check if vehicle reporting current location already exists on database
+
+  const filteredVehicle = vehicles.filter(vehicle => vehicle.vehicleId === vehicleId)
+  if (filteredVehicle.length !== 0) {
+    filteredVehicle[0].path.push([latitude, longitude])
+    filteredVehicle[0].currentPosition = filteredVehicle[0].path.length
+    const currentPosition = filteredVehicle[0].path.length
+
+    return res.json({
+      vehicleId: filteredVehicle[0].vehicleId,
+      currentPosition: filteredVehicle[0].path[currentPosition - 1]
+    })
+  } else { // If the vehicle doesn't exist at database it's created and add to it
+    const path = [latitude, longitude]
+    const newVehicle = new Vehicle([path].length - 1, [path], vehicleId)
+    vehicles.push(newVehicle)
+    const currentPosition = [path].length - 1
+
+    return res.json({
+      vehicleId: newVehicle.vehicleId,
+      currentPosition: newVehicle.path[currentPosition]
+    })
+  }
+}
+
 export const getVehicleLocation = async (req: Request, res: Response) => {
   try {
     const vehicles = vehicleData.map(vehicle => {
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,6 @@
 import express, { Response, Request } from 'express'
 
-import { getVehicleLocation } from '../controllers/location-monitoring/getVehicleLocation.js'
+import { getVehicleLocation, reportCurrentLocation } from '../controllers/location-monitoring/getVehicleLocation.js'
 
 const server = express()
 const serverPort = 3000
@@ -8,13 +8,7 @@ const serverPort = 3000
 server.use(express.json())
 server.use(express.urlencoded({ extended: true }))
 
-server.post('/send-current-location', async (req: Request, res: Response) => {
-  // car's geographic data and id
-  const { vehicleId, longitude, latitude } = req.body
-
-  // const reportLocation = await vehicleReportLocation(vehicleId, longitude, latitude) // method to report location from vehicle
-
-})
+server.post('/report-current-location', reportCurrentLocation)
 
 server.get('/get-one-vehicle-location', getVehicleLocation)
 
